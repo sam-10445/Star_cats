7627edf7f57ff313d2692c36c1a0b3cc7693a8ea
Sam <smirandanovaes@gmail.com>
2025-12-28 13:30:06 -0300

    Adição do menu e do mapa

---
+++ star_cats_imagens.py
@@ -0,0 +1,105 @@
+import pygame
+
+import pygame as pg 
+pygame.init()
+janela = pygame.display.set_mode((1000, 700))
+clock = pygame.time.Clock()
+
+mouse_x, mouse_y = 0,0
+tela_mapa = False
+
+# CARREGAMENTO DAS IMAGENS
+#menu
+menu = pygame.image.load('imagens/menu.png')
+menu = menu.convert()
+menu = pygame.transform.scale(menu, (1000, 700))
+
+#black (sem fundo)
+black = pygame.image.load('imagens/black_sem_fundo.png')
+black = black.convert_alpha()
+black = pygame.transform.scale(black, (52,35))
+black = pygame.transform.flip(black, True, False) #imagem, inverter horizontalmente?, inverter verticalmente?
+
+#mapa
+mapa = pygame.image.load('imagens/mapa.png') 
+mapa = mapa.convert_alpha()
+mapa = pygame.transform.scale(mapa, (1000, 700))
+
+#passaro
+passaro = pygame.image.load('imagens/passaro.png')
+passaro = passaro.convert()
+passaro = pygame.transform.scale(passaro, (1000, 700))
+
+#peixe
+peixe = pygame.image.load('imagens/peixe.png')
+peixe.convert()
+peixe = pygame.transform.scale(peixe, (1000, 700))
+
+#cao
+cao = pygame.image.load('imagens/cao.png')
+cao.convert()
+cao = pygame.transform.scale(cao, (1000, 700))
+
+#loja
+loja = pygame.image.load('imagens/loja.png')
+loja.convert()
+loja = pygame.transform.scale(loja, (1000, 700))
+
+#tela de derrota
+tela_de_derrota = pygame.image.load('imagens/tela_de_derrota.png')
+tela_de_derrota.convert()
+tela_de_derrota = pygame.transform.scale(tela_de_derrota, (1000, 700))
+
+#tela de vitoria
+tela_de_vitoria = pygame.image.load('imagens/tela_de_vitoria.png')
+tela_de_vitoria.convert()
+tela_de_vitoria = pygame.transform.scale(tela_de_vitoria, (1000, 700))
+
+#mouse (sem fundo)
+mouse = pygame.image.load('imagens/mouse_sem_fundo.png')
+mouse = mouse.convert_alpha()
+mouse = pygame.transform.scale(mouse, (52,35))
+
+#ITENS (com fundo)
+#leite
+leite = pygame.image.load('imagens/iten_leite.png')
+leite = leite.convert()
+leite = pygame.transform.scale(leite, (25.6,26.3))
+
+#sardinha
+sardinha = pygame.image.load('imagens/iten_sardinha.png')
+sardinha = sardinha.convert()
+sardinha = pygame.transform.scale(sardinha, (35.8, 33.3))
+
+#biscoito de peixe
+biscoito_de_peixe = pygame.image.load('imagens/iten_peixe.png')
+biscoito_de_peixe = biscoito_de_peixe.convert()
+biscoito_de_peixe = pygame.transform.scale(biscoito_de_peixe, (30, 22.1))
+
+#PATAS (sem fundo)
+#pata preta
+pata_preta = pygame.image.load('imagens/pata_preta_sem_fundo.png')
+pata_preta = pata_preta.convert_alpha()
+pata_preta = pygame.transform.scale(pata_preta, (220, 209))
+
+#pata azul
+pata_azul = pygame.image.load('imagens/pata_azul_sem_fundo.png')
+pata_azul = pata_azul.convert_alpha()
+pata_azul = pygame.transform.scale(pata_azul, (211.5, 209))
+
+#pata vermelha
+pata_vermelha = pygame.image.load('imagens/pata_vermelha_sem_fundo.png')
+pata_vermelha = pata_vermelha.convert_alpha()
+pata_vermelha = pygame.transform.scale(pata_vermelha, (195.5, 209))
+
+#quadrado = pygame.Surface([30, 30]) # cria quadrado com 30 pixels de lado
+font = pygame.font.Font(None, 24) #definir fonte
+surface_texto = font.render(f"Aperte enter", True, 'black')
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+
+  pygame.display.flip() # Desenha o quadro atual na tela
+  clock.tick(60)
